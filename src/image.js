b72c11a4be80c843db54a405436ab6672d38a557
Bhanu Pratap Mahato <40088654+roshanraj12363@users.noreply.github.com>
2021-08-26 19:02:52 +0530

    Add files via upload

---
+++ src/image.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import img1 from './images/phone.png';
+import "./components/mystyle.css";
+
+function Images(){
+    return(
+        <div>
+            <img src={img1} alt="" className="mob"/>
+        </div>
+    )
+
+}
+
+export default Images;
